d57496cf41b1d20838cf19110ba6c21573712fb6
chriswmercer <chriswm@me.com>
2019-01-12 13:15:41 +0000

    Update isogram.c
    Update to improve readability and to make user of isalpha

--- isogram/src/isogram.c
+++ isogram/src/isogram.c
@@ -1,26 +1,30 @@
 //
 // Created by cmercer on 08/01/2019.
 //
-#include"isogram.h"
-#include<string.h>
-#include<ctype.h>
-#include<stdio.h>
+#include "isogram.h"
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
 
 bool is_isogram(const char phrase[]) {
-    if(!phrase) {
+    if (!phrase) {
         return false;
     }
-    if(strlen(phrase) == 0) {
+    if (strlen(phrase) == 0) {
         return true;
     }
 
-    unsigned int i = 0, count[26] = {0}, x;
+    unsigned int i = 0;
+    uint32_t letter_mask = 0;
 
     while (phrase[i] != '\0') {
-        char newChar = tolower(phrase[i]);
-        if (newChar >= 'a' && newChar <= 'z') {
-            x = newChar - 'a';
-            if (++count[x] > 1) return false;
+        char newChar = phrase[i];
+        if (isalpha(newChar)) {
+            unsigned int x = 1 << (tolower(newChar) - 'a');
+            if (letter_mask & x) return false;
+            letter_mask |= x;
         }
         i++;
     }
